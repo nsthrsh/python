31a4365ca16c2590378646d9099c5791c0ce5454
nsthrsh <44110480+nsthrsh@users.noreply.github.com>
2020-06-30 13:21:20 +0300

    Add files via upload

---
+++ task1/example1.py
@@ -0,0 +1,11 @@
+a = 5 # int
+b = 7.0 # float
+c = 2 > 4 # boolean
+d = "World" # string
+e = 1.5 + 0.5j # complex
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(a, b, c, d, e.real, e.imag)
---
+++ task1/example2.py
@@ -0,0 +1,19 @@
+a = 5 # int
+b = 7.0 # float
+c = 1+2 # 3
+d = 5-3 # 2
+e = a*b # 35.0
+f = 3.0/2 # 1.5
+g = 3/2 # 1
+h = 5%3 # 2
+j = 10**7.3 # 19952623.1497
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(j))
+print(a, b, c, d, e, f, g, h, j)
